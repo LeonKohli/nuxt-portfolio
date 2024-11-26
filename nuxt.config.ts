ce2e6233f16bc8653e94b54780193f326e4425c1
LeonKohli <98176333+LeonKohli@users.noreply.github.com>
2024-11-27 00:15:17 +0100

    feat: configure font settings in nuxt.config.ts with Google Fonts provider and experimental CSS variable processing

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
     '@nuxt/icon',
-    '@nuxt/fonts',
+    '@nuxt/fonts', 
     '@nuxtjs/tailwindcss',
     '@nuxtjs/color-mode',
     '@nuxt/image',
@@ -23,5 +23,22 @@ export default defineNuxtConfig({
   colorMode: {
     classPrefix: '',
     classSuffix: '',
+  },
+  fonts: {
+    defaults: {
+      weights: [400, 500, 600, 700],
+      styles: ['normal'],
+      subsets: ['latin']
+    },
+    families: [
+      {
+        name: 'Exo 2',
+        provider: 'google'
+      }
+    ],
+    provider: 'google',
+    experimental: {
+      processCSSVariables: true
+    }
   }
 })
