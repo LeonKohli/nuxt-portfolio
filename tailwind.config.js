6319bb697cb9fa5e5dc257ff0a02adedee3564f6
LeonKohli <98176333+LeonKohli@users.noreply.github.com>
2024-12-08 23:48:18 +0100

    refactor: add custom opacity value for improved styling options

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,9 @@ module.exports = {
       fontFamily: {
         exo: ["Exo"],
       },
+      opacity: {
+        '0': '0.001'
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
